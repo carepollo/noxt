b939a0e368cb8697ef72632b18b14a6fb7b9637f
ChickenFace <carepollo6000@gmail.com>
2024-06-03 17:25:25 -0500

    parsing path parameters

--- router/router.go
+++ router/router.go
@@ -1,34 +1,7 @@
 package router
 
-import (
-	"fmt"
-	"net/http"
-	"strings"
-)
-
 type Router struct {
-	// routes Tree[Handler]
-}
-
-func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := router.parseUrl(r.URL.Path)
-
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, strings.Join(path, "/"))
-	// method := r.Method
-
-	// req := Request{
-	// 	Method:      method,
-	// 	QueryParams: make(map[string]string),
-	// 	UrlParams:   make(map[string]string),
-	// 	Headers:     make(map[string]string),
-	// }
-}
-
-func (router *Router) parseUrl(url string) []string {
-	cleaned := strings.Trim(url, "/")
-	res := strings.Split(cleaned, "/")
-	return res
+	routes Tree[Handler]
 }
 
 type Handler func(Request) Response
--- router/tree.go
+++ router/tree.go
@@ -1,36 +1,43 @@
+/*
+*
+TODO:
+- have to evaluate when it starts with '/:' such case is when there is only one first path
+- have to be able to resolve /:var1/:var2 current behaviour stack overflow
+*
+*/
 package router
 
 import "strings"
 
-type Tree struct {
-	root *Node
+type Tree[T any] struct {
+	root *Node[T]
 }
 
-type Node struct {
+type Node[T any] struct {
 	prefix   string
-	children []*Node
+	children []*Node[T]
 }
 
-func NewTree() *Tree {
-	return &Tree{
-		root: &Node{},
+func NewTree[T any]() *Tree[T] {
+	return &Tree[T]{
+		root: &Node[T]{},
 	}
 }
 
-func (tree *Tree) Insert(word string) {
+func (tree *Tree[T]) Insert(word string) {
 	tree.insertNode(tree.root, word)
 }
 
-func (tree *Tree) insertNode(node *Node, word string) {
+func (tree *Tree[T]) insertNode(node *Node[T], word string) {
 	for _, child := range node.children {
 		commonPrefix := tree.getCommonPrefix(child.prefix, word)
 		if len(commonPrefix) > 0 {
 			if commonPrefix == child.prefix {
 				tree.insertNode(child, word[len(commonPrefix):])
 			} else {
-				newNode := &Node{
+				newNode := &Node[T]{
 					prefix:   commonPrefix,
-					children: []*Node{child},
+					children: []*Node[T]{child},
 				}
 
 				child.prefix = child.prefix[len(commonPrefix):]
@@ -44,13 +51,13 @@ func (tree *Tree) insertNode(node *Node, word string) {
 	}
 
 	// no common prefix, create a new child
-	newNode := &Node{
+	newNode := &Node[T]{
 		prefix: word,
 	}
 	node.children = append(node.children, newNode)
 }
 
-func (tree *Tree) removeNode(nodes []*Node, node *Node) []*Node {
+func (tree *Tree[T]) removeNode(nodes []*Node[T], node *Node[T]) []*Node[T] {
 	for i, n := range nodes {
 		if n == node {
 			return append(nodes[:i], nodes[i+1:]...)
@@ -61,7 +68,7 @@ func (tree *Tree) removeNode(nodes []*Node, node *Node) []*Node {
 }
 
 // returns the common prefix of two strings.
-func (tree *Tree) getCommonPrefix(origin, target string) string {
+func (tree *Tree[T]) getCommonPrefix(origin, target string) string {
 	minLen := len(origin)
 	if len(target) < minLen {
 		minLen = len(target)
@@ -76,11 +83,11 @@ func (tree *Tree) getCommonPrefix(origin, target string) string {
 	return origin[:minLen]
 }
 
-func (tree *Tree) Search(word string) *Node {
+func (tree *Tree[T]) Search(word string) *Node[T] {
 	return tree.searchNode(tree.root, word)
 }
 
-func (tree *Tree) searchNode(node *Node, word string) *Node {
+func (tree *Tree[T]) searchNode(node *Node[T], word string) *Node[T] {
 	if len(word) == 0 {
 		return node
 	}
@@ -88,6 +95,23 @@ func (tree *Tree) searchNode(node *Node, word string) *Node {
 	for _, child := range node.children {
 		if strings.HasPrefix(word, child.prefix) {
 			return tree.searchNode(child, word[len(child.prefix):])
+		}
+
+		if strings.HasPrefix(child.prefix, ":") {
+			nextPath := strings.Index(word, "/")
+			if nextPath == -1 {
+				return child
+			} else {
+				prefix := ""
+				val := strings.Index(child.prefix, "/")
+				if val == -1 {
+					prefix = child.prefix
+				} else {
+					prefix = child.prefix[:val]
+				}
+
+				return tree.searchNode(node, prefix+word[nextPath:])
+			}
 		}
 	}
 
